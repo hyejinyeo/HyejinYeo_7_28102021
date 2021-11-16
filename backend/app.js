701aa66631c2889a66a4049845be4bf947c9283f
Hyejin <hj_yeo@naver.com>
2021-11-16 20:36:06 +0100

    Backend: Sequelize, mysql2, cors

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const cors = require('cors');
+
+/* Base de données */
 
 
 /* Création d'une application Express */
 const app = express();
+
+
+// parse requests of content-type - application/json
+app.use(express.json());
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(express.urlencoded({ extended: true }));
+
 
 app.use((req, res, next) => {
     console.log('Request received');
